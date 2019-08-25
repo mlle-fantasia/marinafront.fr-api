1e438f3ac536bb8f498a27280057bc0607db774b
marina63 <marinafront@hotmail.fr>
2019-08-25 22:00:30 +0200

    authentification ok

--- src/controller/AuthentificationAction.ts
+++ src/controller/AuthentificationAction.ts
@@ -2,28 +2,39 @@ import {Request, Response} from "express";
 import {getManager, getRepository} from "typeorm";
 import {User} from "../entity/User";
 
+const bcrypt = require('bcrypt');
 
 /**
  * Saves given post.
  */
 export async function authAction(request: Request, response: Response) {
     const userRepository = getManager().getRepository(User);
-    console.log(request.body.login);
+    console.log(request.body.pass);
     const user = await userRepository.find({
         where: {
             email: request.body.login
         }
     });
 
-    if(user[0]){
-        console.log(user);
-        response.send("ok");
-    }else{
-        response.status(404);
-        response.end("utilisateur non trouvé");
-        return;
+    bcrypt.hash(request.body.pass, 10, function(err, hash) {
+        console.log("1",hash)
+    });
 
-    }
+    let hash = user[0].password;
+    bcrypt.compare(request.body.pass, hash).then(function(res) {
+        console.log("2",hash);
+        console.log("res " , res);
+        if(res ){
+            response.send("ok");
+        }
+        else{
+            response.status(404);
+            response.send("pas ok");
+            return;
+
+        }
+    });
+
 
 
 }
