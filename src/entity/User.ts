bdf6aeebf17d6c56a90e70ad19378f2291d28f67
Marina Front <marinafront@hotmail.fr>
2020-08-19 08:27:53 +0200

    config orm

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,25 +1,22 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
-
-@Entity({name: "users", })
+@Entity({ name: "users" })
 export class User {
+	@PrimaryGeneratedColumn({ name: "us_id" })
+	id: number;
 
-    @PrimaryGeneratedColumn({name: "us_id"})
-    id: number;
+	@Column({ name: "us_email" })
+	email: string;
 
-    @Column({name: "us_email"})
-    email: string;
+	@Column({ name: "us_password", type: "text" })
+	password: string;
 
-    @Column({name: "us_password", type:"text", } )
-    password: string;
+	@Column({ name: "us_tel" })
+	tel: string;
 
-    @Column({name: "us_tel"})
-    tel: string;
+	@Column({ name: "us_adress" })
+	address: string;
 
-    @Column({name: "us_adress"})
-    adress: string;
-
-    @Column({name: "us_city"})
-    city: string;
-
+	@Column({ name: "us_city" })
+	city: string;
 }
